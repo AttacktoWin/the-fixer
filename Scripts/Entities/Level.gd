1586e914d59a0d9df2d9fe19084b8e8156487120
Yarrow <42461443+NosebleedPlant@users.noreply.github.com>
2023-03-27 13:19:38 -0600

    check point

--- Scripts/Entities/Level.gd
+++ Scripts/Entities/Level.gd
@@ -1,13 +1,7 @@
 # Author: Marcus
 
 extends Node2D
-onready var nod:ColorRect = get_node("%ColorRect")
 
 func _ready():
 	Scene.set_root(self)
-
-func _process(_delta):
-	var t = get_viewport().get_mouse_position()
-	t.x/=get_viewport_rect().size.x
-	t.y/=get_viewport_rect().size.y
-	
+#	Scene._player = Scene._root.get_node("Level/Generator").get_node("%Player")
