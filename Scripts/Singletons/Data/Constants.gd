961caf35ff7610d55a5b5f57d20632461ae0efd8
MdderUofA <mdder@ualberta.ca>
2023-03-28 01:39:12 -0600

    Use inner classes for constants

--- Scripts/Singletons/Data/Constants.gd
+++ Scripts/Singletons/Data/Constants.gd
@@ -1,24 +1,27 @@
 class_name Constants extends Node2D
 
-const TILE = {SIDE = 64, HYP = sqrt(64 * 64 * 2)}
 
-const SP = {
-	B_FLASH = "flash",
-	C_FLASH_COLOR = "flash_color",
-	C_LINE_COLOR = "line_color",
-	I_LINE_THICKNESS = "line_thickness"
-}
+class TILE:
+	const SIDE = 64
+	const HYP = sqrt(SIDE * SIDE * 2)
 
-const COLOR = {
-	YELLOW = Color(252 / 255.0, 216 / 255.0, 55 / 255.0),
-	RED = Color(255 / 255.0, 30 / 255.0, 43 / 255.0),
-	WHITE = Color(1.0, 1.0, 1.0, 1.0),
-	BLACK = Color(0.0, 0.0, 0.0, 1.0),
-	INVISIBLE = Color(1.0, 1.0, 1.0, 0.0)
-}
 
-const TILE_TYPE = {
-	VOID = -1,
-	FLOOR = 1,
-	WALL = 2,
-}
+class SP:
+	const B_FLASH = "flash"
+	const C_FLASH_COLOR = "flash_color"
+	const C_LINE_COLOR = "line_color"
+	const I_LINE_THICKNESS = "line_thickness"
+
+
+class COLOR:
+	const YELLOW = Color(252 / 255.0, 216 / 255.0, 55 / 255.0)
+	const RED = Color(255 / 255.0, 30 / 255.0, 43 / 255.0)
+	const WHITE = Color(1.0, 1.0, 1.0, 1.0)
+	const BLACK = Color(0.0, 0.0, 0.0, 1.0)
+	const INVISIBLE = Color(1.0, 1.0, 1.0, 0.0)
+
+
+class TILE_TYPE:
+	const VOID = -1
+	const FLOOR = 1
+	const WALL = 2
