cd68caa1d8572dcb051126061d5e11995f8c798e
MdderUofA <mdder@ualberta.ca>
2023-03-28 02:41:54 -0600

    Skeleton for bullet hit

--- Scripts/Entities/Attacks/BulletBase.gd
+++ Scripts/Entities/Attacks/BulletBase.gd
@@ -1,6 +1,11 @@
 class_name BulletBase extends BaseAttack
 
 
+func _on_hit_entity(_entity: LivingEntity):
+	pass
+	#Wwise.post_event_id(AK.EVENTS.HIT_PISTOL_PLAYER, self._damage_source)
+
+
 func _physics_process(_delta):
 	._physics_process(_delta)
 	events.invoke(EVENTS.MOVE, null)
