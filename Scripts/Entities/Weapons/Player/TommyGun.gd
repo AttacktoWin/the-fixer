e4f8d9f6226b3bdb1a9f91c48f86ff1634f2cf75
MdderUofA <mdder@ualberta.ca>
2023-03-29 01:21:52 -0600

    More pierce for tommy gun

--- Scripts/Entities/Weapons/Player/TommyGun.gd
+++ Scripts/Entities/Weapons/Player/TommyGun.gd
@@ -4,6 +4,7 @@ class_name PlayerGunTommy extends PlayerBaseGun
 
 export var fire_count: int = 3
 export var fire_repeat_speed: float = 0.05
+export var gun_pierce_override: int = 2
 
 var _fire_counter = 0
 var _fire_repeat_timer = 0
@@ -12,6 +13,7 @@ var _fire_repeat_timer = 0
 func _fire_impl(direction: float):
 	var bullet: BulletBase = _generate_bullet()
 	bullet.set_direction(direction + rand_range(-bullet_spread, bullet_spread))
+	bullet.max_pierce = self.gun_pierce_override
 	CameraSingleton.shake(self.screen_shake_on_fire)
 	AI.notify_sound(self.global_position)
 
@@ -22,7 +24,7 @@ func _cooldown_timer_tick(delta):
 		if self._fire_repeat_timer < 0:
 			self._fire_repeat_timer += fire_repeat_speed
 			self._fire_counter -= 1
-			
+
 			Wwise.post_event_id(self.sound_fire, self.entity)
 			self._fire_impl(_get_fire_angle())
 	else:
