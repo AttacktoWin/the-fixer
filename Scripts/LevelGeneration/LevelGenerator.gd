f5bbb8e52df67e7a2d4bac98de84b51c48f582dd
Yarrow <42461443+NosebleedPlant@users.noreply.github.com>
2023-04-08 18:17:26 -0600

    room generation working

--- Scripts/LevelGeneration/LevelGenerator.gd
+++ Scripts/LevelGeneration/LevelGenerator.gd
@@ -1,110 +1,99 @@
 extends Node2D
-
-export(NodePath) var Player_path
-export(NodePath) var Exit_path
-onready var player = get_node(Player_path) if Player_path else Scene.player
-onready var exit = get_node(Exit_path)
-
-export(NodePath) var parent
-onready var entities: Node2D = get_node(parent)
-export(int) var enemies_per_room = 5
-export(int) var enemy_buffer = 2
-export(int) var pillbug_spawn_rate = 50
-export(int) var spyder_spawn_rate = 25
-export(int) var beetle_spawn_rate = 15
-export(int) var ant_spawn_rate = 10
-
-export(Resource) var generator_data = PCG_RoomData.new()
-
-onready var Floor = get_node("%Floor")
-onready var Walls: TileMap = get_node("%Walls")
-
-enum MODE { ROOM, WALKED_ROOM }
-enum CARDINAL_DIR { N, S, E, W }
-var rng = RandomNumberGenerator.new()
 var level = []  #-1 = non, 1 = floor, 2 = wall
 var saved_seed
 
+const TILE_SIZE = 10
 
-func _init():
-	rng.randomize()
-	saved_seed = rng.seed
+var room_count:int = 5
+var min_dims:Vector2 = Vector2(100,100)
+var max_dims:Vector2 = Vector2(500,500)
+var rng = RandomNumberGenerator.new()
 
-
+#DEBUG
+var test_rooms = []
+var _debug = true
 func _ready():
-	var path = []
-	var room_list = []
-	var room_centers = []
-	var path_by_rooms = {}
-	var enemy_info = {
-		"pillbug": [pillbug_spawn_rate, load("res://Scenes/Enemies/E_Goomba.tscn")],
-		"spyder": [spyder_spawn_rate, load("res://Scenes/Enemies/E_Spyder.tscn")],
-		"beetle": [beetle_spawn_rate, load("res://Scenes/Enemies/E_Beetle.tscn")],
-		"ant": [ant_spawn_rate, load("res://Scenes/Enemies/E_Ant.tscn")]
-	}
+	self.rng.randomize()
+	self.test_rooms = _spawn_rects()
 
-	#initlize level array
-	initialize_level(generator_data)
-	var level_space = Rect2(0, 0, generator_data.map_size.x, generator_data.map_size.y)
+func _process(delta):
+	var is_seperated =  _seperate(self.test_rooms)
+	if is_seperated and _debug:
+		_shrink(self.test_rooms)
+		_debug = false
+	update()
 
-	#declare building too,
-	var partitioner = PCG_Partioner.new()
-	var corridor_builder = PCG_Corridors.new()
-	var populator = PCG_Populator.new()
-	var filler = PCG_TileFiller.new()
+#DEBUG
+func _draw():
+	for room in self.test_rooms:
+		draw_rect(room,Color(0,1,0,1),false,2)
+	self.test_rooms = _order_room(self.test_rooms)
+	var current = self.test_rooms[0]
+	for i in self.test_rooms.size():
+		draw_line(
+			current.get_center(),
+			self.test_rooms[i].get_center(),
+			Color(1,1,1,1),
+			3
+		)
+		current = self.test_rooms[i]
 
-	#initialize level building tools
-	partitioner.construct(
-		rng,
-		level_space,
-		generator_data.min_width,
-		generator_data.min_height,
-		generator_data.shrink_factor
-	)
-	corridor_builder.construct(rng, generator_data.corridor_width)
-	populator.construct(player, exit, enemies_per_room, enemy_buffer, enemy_info, rng.seed)
-	filler.construct(rng)
-
-	match generator_data.mode:
-		MODE.ROOM:
-			#simple room based builder
-			partitioner.room_builder(path, path_by_rooms, room_list, room_centers)
-			path += corridor_builder.connect_rooms(room_centers)
-		MODE.WALKED_ROOM:
-			#builder with room based random walk for more organic looking rooms
-			var walker = PCG_Walker.new()
-			walker.construct(rng.seed)
-			partitioner.binary_space_partition(
-				generator_data.min_width, generator_data.min_height, level_space, room_list
+func _spawn_rects()->Array:
+	var rooms = []
+	for i in self.room_count:
+		var room = Rect2(
+			Vector2(712,300),
+			Vector2(
+				self.rng.randi_range(self.min_dims.x+TILE_SIZE,self.max_dims.x+TILE_SIZE),
+				self.rng.randi_range(self.min_dims.y+TILE_SIZE,self.max_dims.y+TILE_SIZE)
+				)
 			)
-			room_centers = partitioner.get_centers(room_list)
-			path = corridor_builder.connect_rooms(room_centers)
-			var build_data = walker.random_walk_room(
-				generator_data, path, path_by_rooms, room_list, room_centers
-			)
-			path = build_data[0]
-			path_by_rooms = build_data[1]
+		rooms.append(room)
+	return rooms
 
-	var end_index = populator.populate(
-		Floor, path, path_by_rooms, room_list, room_centers, entities
-	)
+func _seperate(rooms)->bool:
+	if _is_overlapping(rooms):
+		for current in room_count:
+			for other in room_count:
+				if current == other or not rooms[current].intersects(rooms[other],true):
+					continue
+				var move_vec = rooms[current].get_center() - rooms[other].get_center()
+				print(move_vec)
+				if move_vec.x>0:
+					rooms[current].position.x = clamp(rooms[current].position.x+TILE_SIZE,0,INF)
+					rooms[other].position.x = clamp(rooms[other].position.x-TILE_SIZE,0,INF)
+				if move_vec.y>0:
+					rooms[current].position.y = clamp(rooms[current].position.y+TILE_SIZE,0,INF)
+					rooms[other].position.y = clamp(rooms[other].position.y-TILE_SIZE,0,INF)
+		return false
+	return true
 
-	self.level = filler.floor_pass(path, level, Floor)
-	filler.room_deco_pass(path, room_list, path_by_rooms, end_index, Floor)
-	self.level = filler.wall_pass(path, level, Walls)
+func _is_overlapping(rooms:Array)->bool:
+	for current in rooms:
+		for other in rooms:
+			if current != other and current.intersects(other,true):
+				return true
+	return false
 
+func _order_room(rooms):
+	var current = rooms.pop_at(0)
+	var sorted = [current]
+	
+	while rooms.size()>0:
+		current = rooms.pop_at(_closest_room(current,rooms))
+		sorted.append(current)
+	return sorted
 
-func initialize_level(data):
-	for x in range(0, data.map_size.x + 1):
-		self.level.push_back([])
-		for _y in range(0, data.map_size.y + 1):
-			self.level[x].push_back(-1)
+func _closest_room(current:Rect2,rooms)->int:
+	var best_index = 0
+	var min_dist = INF
+	for i in rooms.size():
+		var dist = current.get_center().distance_squared_to(rooms[i].get_center())
+		if dist<min_dist:
+			min_dist = dist
+			best_index = i
+	return best_index
 
-
-func set_seed(value):
-	rng.seed = value
-	saved_seed = value
-
-
-func get_seed():
-	return rng.seed
+func _shrink(rooms):
+	for i in rooms.size():
+		rooms[i].size -= Vector2(TILE_SIZE,TILE_SIZE)
