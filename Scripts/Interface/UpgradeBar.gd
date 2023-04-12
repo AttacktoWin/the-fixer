e3a4ee9231113bdeddbbac527ef0e9c40bd6beaf
Yarrow <42461443+NosebleedPlant@users.noreply.github.com>
2023-04-11 22:23:13 -0600

    removed print flood. setup upgrade ui

--- Scripts/Interface/UpgradeBar.gd
+++ Scripts/Interface/UpgradeBar.gd
@@ -2,13 +2,22 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 var bar_units = []
+var active_units = 0
 func _ready():
 	bar_units = get_children()
-	print(bar_units)
+	set_active(active_units)
 
-func _set_active(count)->bool:
+func set_active(count)->bool:
 	if count>bar_units.size():
 		return false
 	for i in count:
 		bar_units[i].frame = 1
 	return true
+
+func increment():
+	active_units+=1
+	set_active(active_units)
+
+func reset():
+	active_units=0
+	set_active(active_units)
