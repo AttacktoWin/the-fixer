200f378470df1593f2b7915df03eb6b0e50c23dc
MdderUofA <mdder@ualberta.ca>
2023-04-12 23:17:35 -0600

    Manifestation roar

--- Scripts/Enemies/Manifestation/ManifestationStateSpawnWave.gd
+++ Scripts/Enemies/Manifestation/ManifestationStateSpawnWave.gd
@@ -3,11 +3,18 @@
 class_name ManifestationStateSpawnWave extends FSMNode
 
 var _timer = 0
+var _global_timer = 0
 var _spawn_delay = 0.36
 var _spawn_count = 5
 var _current_spawn = 0
 
+var _roar_fx = null
+
 const ATTACK_DELAY = 0.7
+
+
+func initialize():
+	self._roar_fx = Scene.ui.get_node("RoarVFX")
 
 
 func get_handled_states():
@@ -16,6 +23,7 @@ func get_handled_states():
 
 func enter():
 	self._timer = -0.5
+	self._global_timer = 0
 	self._current_spawn = 0
 	Wwise.post_event_id(AK.EVENTS.ROAR_MANIFESTATION, self.entity)
 	self.fsm.set_animation("ROAR")
@@ -29,6 +37,12 @@ func on_anim_reached_end(_anim: String):
 
 func _physics_process(delta):
 	self._timer += delta
+	self._global_timer += delta
+
+	self._roar_fx.modulate.a = MathUtils.interpolate(
+		abs(self._global_timer - 1), 0.4, 0, MathUtils.INTERPOLATE_IN_EXPONENTIAL
+	)
+
 	if self._timer > _spawn_delay:
 		self.entity._spawn_random_enemies(1, false)
 		self._timer -= self._spawn_delay
--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -284,10 +284,10 @@ func _process(_delta):
 			# var scene = load("res://Scenes/Levels/BossRoom.tscn").instance()
 			# TransitionHelper.transition(scene, true, true, 0.01)
 		if Input.is_action_just_pressed("ui_page_down"):
-			# var scene = load("res://Scenes/Levels/BossRoom.tscn").instance()
-			# TransitionHelper.transition(scene, true, true, 0.01)
-			for enemy in AI.get_all_enemies():
-				enemy.queue_free()
+			var scene = load("res://Scenes/Levels/BossRoom.tscn").instance()
+			TransitionHelper.transition(scene, true, true, 0.01)
+			# for enemy in AI.get_all_enemies():
+			# 	enemy.queue_free()
 		if Input.is_action_just_pressed("ui_page_up"):
 			#apply_upgrades([HomingUpgrade.new()])
 			self.kill()
