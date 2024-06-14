36ed4911a192cd6ba9e1620d4d37871ab8e4d538
MdderUofA <mdder@ualberta.ca>
2024-06-13 22:06:08 -0600

    Controller support on settings and tutorial fixes

--- Scripts/Level/Enemies_Remaing.gd
+++ Scripts/Level/Enemies_Remaing.gd
@@ -15,7 +15,7 @@ func _reload():
 		Scene.exit.connect("on_clear_display", self, "_clear")
 
 func _input(_event):
-	if Input.is_action_just_pressed("show_enemies") and Scene.exit:
+	if Input.is_action_just_pressed("show_enemies") and Scene.exit and not PausingSingleton.is_paused():
 		_show()
 	if Input.is_action_just_released("show_enemies") and Scene.exit:
 		_clear()
